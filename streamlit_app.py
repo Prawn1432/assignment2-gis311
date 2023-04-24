e6b56220ee69b48730e404bff1a3b135907359dc
Prawn1432 <130056071+Prawn1432@users.noreply.github.com>
2023-04-24 10:09:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,13 @@ airlines_column_names = ['Airlines_ID','Name','Alias','IATA','ICAO','Callsign','
 airlines.columns = airlines_column_names
 #now we want to only keep the coumns we interested in 
 airlines = airlines.drop(['Alias','IATA','ICAO','Callsign'],axis = 1)
-# now we are going to try clean up our second dataset 
+# now we are going to try clean up our second dataset Airports
+
 airports = pd.read_csv("airports.dat",header = None)
+airports_column_names = ['Airport_ID','Name','City','Country','IATA','ICAO','Latitude','Longitude','Altitude','Timezone','DST','Tz Database Time Zone','Type','Source']
+airports.columns = airports_column_names
+#now we drop some of our airports columns
+airports = airports.drop(['DST','Tz Database Time Zone','Type','Source','Timezone'], axis = 1)
 
 ##
 

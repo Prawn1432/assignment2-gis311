e4d3105ba2ea47a703aaac0ebae58debab1aa3bb
Prawn1432 <130056071+Prawn1432@users.noreply.github.com>
2023-04-24 10:00:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,23 @@ import pandas as pd
 import numpy as np 
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
+# here is the title of what we want to show for our application 
 st.title(" libraries not installed but stream lit works")
+##
+# here we clean our AIRLINES data frame and se what data is inside of it
 airlines = pd.read_csv('airlines.dat',header=None)
 airlines_column_names = ['Airlines_ID','Name','Alias','IATA','ICAO','Callsign','Country','Active']
 airlines.columns = airlines_column_names
 #now we want to only keep the coumns we interested in 
 airlines = airlines.drop(['Alias','IATA','ICAO','Callsign'],axis = 1)
-st.write('the airlines file has been read')
-st.table(airlines)
+# now we are going to try clean up our second dataset 
+airports = pd.read_csv("airports.dat",header = None)
 
+##
+
+# here is the out put of all our data frame
+st.table(airlines)
+st.table(airports)
+
+# this is just to check if our dataframe is read correctly 
+st.write('the airlines file has been read')

d1d3819364b6af7df63c9e252537287a938da0f6
Prawn1432 <130056071+Prawn1432@users.noreply.github.com>
2023-04-24 09:10:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
 #pip install git+https://github.com/pytorch/pytorch.git@v1.9.0
 
 #we first want to import all our libraries
-#import pandas as pd 
-#import numpy as np 
-#import geopandas as gpd
-#import matplotlib.pyplot as plt
-#from shapely.geometry import Point, Polygon
-#import numpy as np
-#from bs4 import BeautifulSoup
-#import csv as csv
+import pandas as pd 
+import numpy as np 
+import geopandas as gpd
+import matplotlib.pyplot as plt
 import streamlit as st
 st.title(" libraries not installed but stream lit works")

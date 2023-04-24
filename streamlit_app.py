37f28c740aa4dce3ff128abf6e26c4b5ae00d626
Prawn1432 <130056071+Prawn1432@users.noreply.github.com>
2023-04-24 09:17:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 #pip install git+https://github.com/pytorch/pytorch.git@v1.9.0
-
+import streamlit as st
 #we first want to import all our libraries
 #import pandas as pd 
 #import numpy as np 
 #import geopandas as gpd
 import matplotlib.pyplot as plt
-import streamlit as st
+
 st.title(" libraries not installed but stream lit works")

99fb895deabfe5be7f63211d0c0e1db1bea67eb4
Prawn1432 <130056071+Prawn1432@users.noreply.github.com>
2023-04-24 12:11:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,14 +44,13 @@ st.subheader("Here We Also Have An Interesting Bar Chart Of All The Airlines in 
 st.write(" Can You guess which country has the most airlines in the world? ")
 st.bar_chart(airlines2,x = 'Country', y='Airlines_ID')
 st.write(" The united kingdom of course has the most airlines in the world, Now how about that! :)")
-
 ########################################################
 # now we are going to try clean up our second dataset Airports
 st.title("Now we are going to look at some interesting facts about planes")
-# first we are going to load in all of our data
-#planes = pd.read_csv('planes.dat',header = None)
-# Now wea assign some column names
-#plane_column_names = ['Plane Model Name','ISO_code','Daffif_code']
-#planes.columns = plane_column_names
-#st.table(planes)
+# first we are going to load in all of our PLanes data
+planes = pd.read_csv('planes.dat',header = None)
+#Now wea assign some column names
+plane_column_names = ['Plane Model Name','ISO_code','Daffif_code']
+planes.columns = plane_column_names
+st.table(planes)
 

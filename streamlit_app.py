0deb3735f39688a3a0e0a8cb225293cd1f8e49ed
Prawn1432 <130056071+Prawn1432@users.noreply.github.com>
2023-04-24 09:27:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 #pip install git+https://github.com/pytorch/pytorch.git@v1.9.0
 import streamlit as st
-#we first want to import all our libraries
-#import pandas as pd 
-#import numpy as np 
-#import geopandas as gpd
+we first want to import all our libraries
+import pandas as pd 
+import numpy as np 
+import geopandas as gpd
 import matplotlib.pyplot as plt
 
 st.title(" libraries not installed but stream lit works")
+airlines = pd.read_csv('airlines.dat'header=None)
+st.write('the airlines file has been read')
